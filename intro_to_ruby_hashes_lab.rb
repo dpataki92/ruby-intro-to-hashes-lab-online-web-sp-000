93b61c93f64f2ce642e9d566b66e71e495124924
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-24 22:12:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,15 +15,13 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: {
-		pieces: 4
+		pieces: 4,
+		names: {}
 		}}
 end
 
 def monopoly_with_third_tier
-	monopoly = {railroads: {
-		pieces: 4,
-		names: {}
-		}}
+	
 end
 
 def monopoly_with_fourth_tier
